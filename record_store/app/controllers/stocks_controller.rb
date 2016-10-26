70c539b24b90ac69c2070ae724c85dc01189801f
Phil Crooks <pacman@crooks.me.uk>
2016-10-26 11:10:16 +0100

    stock retrieval working

--- record_store/app/controllers/stocks_controller.rb
+++ record_store/app/controllers/stocks_controller.rb
@@ -2,7 +2,12 @@ class StocksController < ApplicationController
 
   def index
     stocks = Stock.all()
-    render({ json: stocks.as_json({except: [:created_at, :updated_at], include: [:albums]})})
+    render({ json: stocks.as_json(
+      {
+        except: [:created_at, :updated_at],
+        include: [album: {except: [:created_at, :updated_at]}]
+      }
+    )})
   end
 
 end
